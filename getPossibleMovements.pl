5ad47a24fa51dad86d82966707b3eb127962ec95
Salomé <salome.welche@laposte.net>
2015-06-06 14:00:53 +0200

    getPossible Ameliorations ..

--- getPossibleMovements.pl
+++ getPossibleMovements.pl
@@ -1,3 +1,5 @@
+:- include('ordonner.pl').
+
 %getPossibleMovementL(+B, +N, -ListPossibleMovements, +P ) 
 getPossibleMovementL(B, [], _, L, 0]).
 getPossibleMovementL(N, _, [], L, 1]).
@@ -8,10 +10,14 @@ getPossibleMovementL(N, B, [X|R], [L|R2], 1]) :- getPossibleMovementX(X, B, N, L
 
 
 
-%FAUT ENLEVER LE SWAP DE D(....)
 %getPossibleMovementX(+FromX, +B, +N, -ListPossibleMovements, P ) 
 getPossibleMovementX(X, B, N, [X2|R], 1):- d(B, N, X, X2, 1), getPossibleMovement(X, B, N, R, 1).
 
 getPossibleMovementX(X, B, N, [X2|R], 1):- not(d(B, N, X, X2, 1)), getPossibleMovement(X, B, N, [X2|R], 1).
 
 getPossibleMovementX(X, B, N, [X2|R], 0):- d(N, B, X, X2, 0), getPossibleMovement(X, N, B, R, 0).getPossibleMovementX(X, B, N, [X2|R], 0):- not(d(N, B, X, X2, 0)), getPossibleMovement(X, B, N, [X2|R], 0).
+
+
+%test de deplacement
+d(L1, L2, A, B, 0, L4):-member(A, L1), deplacement(A,B), not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4).
+d(L1, L2, A, B, 1, L4):-member(A, L2), deplacement(A,B), not(member(B, L1)), not(member(B, L2)), swap(L2, A, B, L3), ordonner(L3,L4).
