9d66b017da06c63c8c1609003baa41fe9094ee6e
Salomé <salome.welche@laposte.net>
2015-06-10 14:27:18 +0200

    mise a jour

--- getPossibleMovements.pl
+++ getPossibleMovements.pl
@@ -13,31 +13,26 @@ getPossibleMovementL(N, B, [X|R], [L|R2], 1) :-
 
 
 %getPossibleMovementX(+FromX, +B, +N, -ListPossibleMovements, P ) 
-getPossibleMovementX(X, B, N, [X2|R], 1):- d(B, N, X, X2, 1),
- 	getPossibleMovement(X, B, N, R, 1).
+getPossibleMovementX(X, B, N, [X2|R], 1):- not(member(X2, R)), d(B, N, X, X2, 1),
+ 	getPossibleMovementX(X, B, N, R, 1).
 
-getPossibleMovementX(X, B, N, [X2|R], 1):- not(d(B, N, X, X2, 1)),
- 	getPossibleMovement(X, B, N, [X2|R], 1).
+getPossibleMovementX(X, B, N, [X2|R], 1):- not(member(X2, R)), not(d(B, N, X, X2, 1)),
+ 	getPossibleMovementX(X, B, N, [X2|R], 1).
 
-getPossibleMovementX(X, B, N, [X2|R], 0):- d(N, B, X, X2, 0), 
-	getPossibleMovement(X, N, B, R, 0).
-getPossibleMovementX(X, B, N, [X2|R], 0):- not(d(N, B, X, X2, 0)),
- 	getPossibleMovement(X, B, N, [X2|R], 0).
+getPossibleMovementX(X, B, N, [X2|R], 0):- not(member(X2, R)), d(N, B, X, X2, 0), 
+	getPossibleMovementX(X, N, B, R, 0).
+getPossibleMovementX(X, B, N, [X2|R], 0):-not(member(X2, R)), not(d(N, B, X, X2, 0)),
+ 	getPossibleMovementX(X, B, N, [X2|R], 0).
 
 
 
 %test de deplacement
-d(L1, L2, A, B, 0, L4):-member(A, L1), deplacement(A,B), 
-	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4).
-d(L1, L2, A, B, 1, L4):-member(A, L2), deplacement(A,B),
- 	not(member(B, L1)), not(member(B, L2)), swap(L2, A, B, L3), ordonner(L3,L4).
+d(L1, L2, A, B, 0, L4):-member(A, L1),!, deplacement(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d(L1, L2, A, B, 1, L4):-member(A, L2),!, deplacement(A,B),
+ 	not(member(B, L1)), not(member(B, L2)), swap(L2, A, B, L3), ordonner(L3,L4),!.
 
 %fct de déplacement
-deplacement1(A,B):- A is B+1,A<46,(A%10)<6,!.
-deplacement2(A,B):- A=:=B+10,A<46,(A%10)<6,!.
-deplacement3(A,B):- A=:=B+11,A<46,(A%10)<6,!.
-deplacement4(A,B):- A=:=B+9,A<46,(A%10)<6,!.
-deplacement5(A,B):- A=:=B-1,,A>0,(A%10)<6,!.
-deplacement6(A,B):- A=:=B-10,A>0,(A%10)<6,!.
-deplacement7(A,B):- A=:=B-11,A>0,(A%10)<6,!.
-deplacement8(A,B):- A=:=B-9,A>0,(A%10)<6,!.
+deplacement(A,B):- ((A is B+1,A<46;A is B+10),(A<46,(A mod 10)<6);(A is B+11,A<46,(A mod 10)<6);
+(A is B+9,A<46,(A mod 10)<6);(A is B-1,A>0,(A mod 10)<6,!);( A is B-10,A>0,(A mod 10)<6,!);
+(A is B-11,A>0,(A mod 10)<6,!);(A is B-9,A>0,(A mod 10)<6,!)),!.
--- getPossibleMvtV2.pl
+++ getPossibleMvtV2.pl
@@ -1,2 +1,8 @@
 %fct de deplacement
-deplacement1(A,B):- A is A mod B.
+deplacement(A,B):- A=:=B+10,A<46,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B+11,A<46,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B+9,A<46,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B-1,A>0,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B-10,A>0,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B-11,A>0,(A mod 10)<6,!.
+deplacement(A,B):- A=:=B-9,A>0,(A mod 10)<6,!.
