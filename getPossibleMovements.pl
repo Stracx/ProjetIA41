e28f0aaced222aa2d644aa58934ef34d8e2f7b52
Salomé <salome.welche@laposte.net>
2015-06-10 22:09:41 +0200

    La liste des deplacements possibles enfin finie !

--- getPossibleMovements.pl
+++ getPossibleMovements.pl
@@ -1,38 +1,65 @@
 :- include('ordonner.pl').
 
-%getPossibleMovementL(+B, +N, -ListPossibleMovements, +P ) 
-getPossibleMovementL(B, [], _, L, 0).
-getPossibleMovementL(N, _, [], L, 1).
-getPossibleMovementL(B, N, L, 0):- getPossibleMovementL(B, B, N, L, 0).
-getPossibleMovementL(B, N, L, 1):- getPossibleMovementL(N, B, N, L, 0).
-getPossibleMovementL(B, [X|R], N, [L|R2], 0) :- 
-	getPossibleMovementX(X, B, N, L, 0), getPossibleMovementL(B, R, N, R2, 0).
-getPossibleMovementL(N, B, [X|R], [L|R2], 1) :- 
-	getPossibleMovementX(X, B, N, L, 1), getPossibleMovementL(N, B, R, R2, 1).
+%getPossibleMovementL(+Joueur, +Joueur, +Adversaire, -ListPossibleMovements ) 
+
+getPossibleMovementL(J, [X1, X2, X3, X4], A, L) :- 
+	getPossibleMovementX(X1, J, A, L1, 8), getPossibleMovementX(X2, J, A, L2, 8), getPossibleMovementX(X3, J, A, L3, 8), getPossibleMovementX(X4, J, A, L4, 8),
+	merge(L1, L2, L22), merge(L3, L4, L44), merge(L22, L44, L).
 
 
 
-%getPossibleMovementX(+FromX, +B, +N, -ListPossibleMovements, P ) 
-getPossibleMovementX(X, B, N, [X2|R], 1):- not(member(X2, R)), d(B, N, X, X2, 1),
- 	getPossibleMovementX(X, B, N, R, 1).
-
-getPossibleMovementX(X, B, N, [X2|R], 1):- not(member(X2, R)), not(d(B, N, X, X2, 1)),
- 	getPossibleMovementX(X, B, N, [X2|R], 1).
-
-getPossibleMovementX(X, B, N, [X2|R], 0):- not(member(X2, R)), d(N, B, X, X2, 0), 
-	getPossibleMovementX(X, N, B, R, 0).
-getPossibleMovementX(X, B, N, [X2|R], 0):-not(member(X2, R)), not(d(N, B, X, X2, 0)),
- 	getPossibleMovementX(X, B, N, [X2|R], 0).
 
 
+%getPossibleMovementX(+Depart, +PionsJoueur, +PionsAdversaire, -ListPossibleMovements, +NumeroIteration ) 
 
-%test de deplacement
-d(L1, L2, A, B, 0, L4):-member(A, L1),!, deplacement(A,B), 
+getPossibleMovementX(_, _, _, [], 0).
+
+getPossibleMovementX(X, B, N, L, 8):- (d1(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 7),!);(not(d1(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 7),!).
+getPossibleMovementX(X, B, N, L, 7):- (d2(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 6),!);(not(d2(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 6),!).
+getPossibleMovementX(X, B, N, L, 6):- (d3(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 5),!);(not(d3(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 5),!).
+getPossibleMovementX(X, B, N, L, 5):- (d4(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 4),!);(not(d4(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 4),!).
+getPossibleMovementX(X, B, N, L, 4):- (d5(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 3),!);(not(d5(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 3),!).
+getPossibleMovementX(X, B, N, L, 3):- (d6(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 2),!);(not(d6(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 2),!).
+getPossibleMovementX(X, B, N, L, 2):- (d7(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 1),!);(not(d7(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 1),!).
+getPossibleMovementX(X, B, N, L, 1):- (d8(B, N, X, _, L2), add(L2, R, L), getPossibleMovementX(X, B, N, R, 0),!);(not(d8(B, N, X, _, _)),getPossibleMovementX(X, B, N, L, 0),!).
+
+
+
+%test de deplacement  d(+PionsJoueur, +PionsAdversaire, +Depart, ?Arrivee, ?ListeArriveeJoueur)
+d1(L1, L2, A, B, L4):-member(A, L1),!, deplacement1(A,B), 
 	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
-d(L1, L2, A, B, 1, L4):-member(A, L2),!, deplacement(A,B),
- 	not(member(B, L1)), not(member(B, L2)), swap(L2, A, B, L3), ordonner(L3,L4),!.
+d2(L1, L2, A, B, L4):-member(A, L1),!, deplacement2(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d3(L1, L2, A, B, L4):-member(A, L1),!, deplacement3(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d4(L1, L2, A, B, L4):-member(A, L1),!, deplacement4(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d5(L1, L2, A, B, L4):-member(A, L1),!, deplacement5(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d6(L1, L2, A, B, L4):-member(A, L1),!, deplacement6(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d7(L1, L2, A, B, L4):-member(A, L1),!, deplacement7(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+d8(L1, L2, A, B, L4):-member(A, L1),!, deplacement8(A,B), 
+	not(member(B, L1)), not(member(B, L2)), swap(L1, A, B, L3), ordonner(L3,L4),!.
+
+
+
 
 %fct de déplacement
-deplacement(A,B):- ((A is B+1,A<46;A is B+10),(A<46,(A mod 10)<6);(A is B+11,A<46,(A mod 10)<6);
-(A is B+9,A<46,(A mod 10)<6);(A is B-1,A>0,(A mod 10)<6,!);( A is B-10,A>0,(A mod 10)<6,!);
-(A is B-11,A>0,(A mod 10)<6,!);(A is B-9,A>0,(A mod 10)<6,!)),!.
+deplacement1(A,B):- B is A+1,(B mod 11)<6,!.
+deplacement2(A,B):- B is A+10,B<46,(B mod 11)<6,!.
+deplacement3(A,B):- B is A+11,B<46,(B mod 11)<6,!.
+deplacement4(A,B):- B is A+9,B<46,(B mod 11)<6,!.
+deplacement5(A,B):- B is A-1, B>0,(B mod 11)<6,!.
+deplacement6(A,B):- B is A-10,B>0,(B mod 11)<6,!.
+deplacement7(A,B):- B is A-11,B>0,(B mod 11)<6,!.
+deplacement8(A,B):- B is A-9,B>0,(B mod 11)<6,!.
+
+
+add(X,L, [X|L]).
+
+
+% fct de swap
+swap([C|R1], A, B, [C|R2]):- C\=A,swap(R1, A, B, R2).
+swap([A|R], A, B, [B|R]).
