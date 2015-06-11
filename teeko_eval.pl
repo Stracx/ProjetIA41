5e054ce40b46ec5b8e6df54dc00ab44e5d67694a
Salomé <salome.welche@laposte.net>
2015-06-11 18:20:11 +0200

    alpha betaaaaaaaaaaaa qui marche pas encore mais bientot

--- teeko_eval.pl
+++ teeko_eval.pl
@@ -7,7 +7,7 @@
 
 % Appel simplifié du prédicat de base
 alphaBeta(J, Adv, Depth, Move, OAdv, Val):- 
-	alphaBeta(J, Adv, Depth, -10000, 10000, Move, OJ, OAdv, Val).
+	alphaBeta(J, Adv, Depth, -10000, 10000, Move, OAdv, Val).
 
 % Cas d'arrêt quand la profondeur Depth atteint 0
 alphaBeta(J, Adv, 0, _Alpha, _Beta, Move, OAdv, Val) :-
@@ -25,7 +25,7 @@ alphaBeta(J, Adv, Prof, Alpha, Beta, Mvmt, OAdv, Value):-
 	getPossibleMovementL(J, Adv, Mvmt),
 	Alpha1 is -Beta,	%necessaire au fonctionnement du negamax
 	Beta1 is -Alpha,
-	Profr1 is Prof - 1,	% on passe au niveau suivant de l'arbre
+	Prof1 is Prof - 1,	% on passe au niveau suivant de l'arbre
 	searchBest(Joueur, Mvmt, Adv, Prof1, Alpha1, Beta1, nil, OAdv, [Move, Value]).
 
 % searchBest(+Joueur, +Adv, +Mvmt, +Profondeur,+Alpha,+Beta,+R,?BestMove)
@@ -33,27 +33,20 @@ alphaBeta(J, Adv, Prof, Alpha, Beta, Mvmt, OAdv, Value):-
 searchBest(_J, _Adv, [], _Prof, Alpha, _Beta, Move, _, [Move,Alpha]) :- 
 	!.
 searchBest(J, Adv, [[X1, X2, X3, X4]|Mvmt], Prof, Alpha, Beta, R, OAdv, BestMove) :-
-	move(Adversaire, From, To, N, NAdversaire),
-	getAdv(Joueur, OtherJoueurR),
-	alphaBeta(OtherJoueurR, Profondeur, NAdversaire, Alpha, Beta, _OtherCoup, OriginalAdversaire, Value)
+	alphaBeta(Adv, [X2, X2, X3, X4], Prof, Alpha, Beta, _AutreCoup, OAdv, Value),
 	Value1 is -Value,		% negamax
-	cutBranches(Joueur,[[Fx,Fy],[Tx,Ty],N], OriginalAdversaire, Value1,Profondeur,Alpha,Beta,Moves,Adversaire,R,BestMove).
+	cutBranches(Joueur,Adv, [X1, X2, X3, X4], OAdv, Value1,Prof,Alpha,Beta,Mvmt,R,BestMove).
 
 
 % cutBranches(+Joueur,+Move,+Value,+Profondeur,+Alpha,+Beta,+Moves,+Adversaire,+_R,+BestMove)
 % Permet de couper certaines branches de l'arbre lorsqu'elle sort des bornes alpha - beta
-cutBranches(Joueur,Move, OriginalAdversaire, Value,Profondeur,Alpha,Beta,ListMoves,Adversaire,_R,BestMove) :-
+cutBranches(J, Adv, Depl, OAdv, Value,Prof,Alpha,Beta,Mvmt,_R,BestMove) :-
 	Alpha < Value,
 	Value < Beta, !,	% structure "si, alors"
-	searchBest(Joueur,ListMoves,Adversaire,Profondeur,Value,Beta,Move, OriginalAdversaire, BestMove).
-cutBranches(Joueur,_Move, OriginalAdversaire, Value,Profondeur,Alpha,Beta,ListMoves,Adversaire,R,BestMove) :-
+	searchBest(J, Adv, Mvmt,Prof,Value,Beta,Move, OAdv, BestMove).
+cutBranches(J, Adv, _Depl, OAdv, Value,Prof,Alpha,Beta,Mvmt,R,BestMove) :-
 	Value =< Alpha, !, % structure "si, alors"
-	searchBest(Joueur,ListMoves,Adversaire,Profondeur,Alpha,Beta,R, OriginalAdversaire, BestMove).
-cutBranches(_Joueur, Move, _, Value, _Profondeur, _Alpha, _Beta, _ListMoves, _Adversaire, _R, [Move, Value]).
+	searchBest(J, Adv, Mvmt,Prof,Alpha,Beta,R, OAdv, BestMove).
+cutBranches(_J, _, Depl, _OAdv, Value, _Prof, _Alpha, _Beta, _LMvmt, _R, [Depl, Value]). 
 
-getFrom(B, [X,Y], L):- convert(I,X,Y), nth0(I, B, L).
-belongTo(B, [X,Y], P):- getFrom(B, [X,Y], L), tailEqual(L, P), !.
-
-/* Adversaire, Coord, N, List, Joueur */
-getPossibleFromAdversaire(B, [X,Y], N, L, P):- belongTo(B,[X,Y], P), getPossibleMovement([X,Y],N,L,P).
 
