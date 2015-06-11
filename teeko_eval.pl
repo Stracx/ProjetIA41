b50e1436bc4beb016da9cdc97fed9f420e36df1a
Salomé <salome.welche@laposte.net>
2015-06-11 15:49:28 +0200

    commit avant la galere

--- teeko_eval.pl
+++ teeko_eval.pl
@@ -28,43 +28,41 @@ getValP(1,-1).
 	score(J, Adv, 0, S1), 
 
 
-% alphaBeta(+Joueur, +Adversaire, +Profondeur +Alpha, +Beta, ?Move, Oj,  +OriginalAdv, ?Value)
+% alphaBeta(+Joueur, +Adversaire, +Profondeur +Alpha, +Beta, ?Move, +OriginalAdv, ?Value)
 % Algorithme de Minmax avec simplification negamax et élagage Alpha-Beta
 %	Plus Profondeur est grand, plus le temps de calcul est élevé
 
 % Appel simplifié du prédicat de base
-alphaBeta(J, Adv, Depth, Move, OJ, OAdv, Val):- 
+alphaBeta(J, Adv, Depth, Move, OAdv, Val):- 
 	alphaBeta(J, Adv, Depth, -10000, 10000, Move, OJ, OAdv, Val).
 
 % Cas d'arrêt quand la profondeur Depth atteint 0
-alphaBeta(J, Adv, 0, _Alpha, _Beta, Move, OJ, OAdv, Val) :-
+alphaBeta(J, Adv, 0, _Alpha, _Beta, Move, OAdv, Val) :-
 	!, score(J, Adv, 0, Val).
 
 % Si Board est la Board original avec laquel on a appelé le prédicat, on ne décrémente pas D et on cherche le meilleur mouvement possible
-alphaBeta(J, Adv, Depth, Alpha, Beta, Mvmt, OJ, OAdv, Value) :-
+alphaBeta(J, Adv, Prof, Alpha, Beta, Mvmt, Adv, Value) :-
 	!, getPossibleMovementL(J, Adv, Mvmt),
 	Alpha1 is -Beta,
 	Beta1 is -Alpha,
-	searchBest(Joueur, Moves, Adversaire, Profondeur, Alpha1, Beta1, nil, Adversaire, [Move, Value]),!.
+	searchBest(J, Adv, Mvmt, Prof, Alpha1, Beta1, nil, OAdv, [Move, Value]),!.
 
 % Si Adversaire et OriginalAdversaire sont différent, on calcul les mouvements possible, on décrémente la profondeur avant de rappeler searchBest
-alphaBeta(Joueur, Profondeur, Adversaire, Alpha, Beta, Move, OriginalAdversaire, Value):-
-	allMove(Adversaire, Joueur, Moves),
+alphaBeta(J, Adv, Prof, Alpha, Beta, Mvmt, OAdv, Value):-
+	getPossibleMovementL(J, Adv, Mvmt),
 	Alpha1 is -Beta,	%necessaire au fonctionnement du negamax
 	Beta1 is -Alpha,
-	Profondeur1 is Profondeur - 1,	% on passe au niveau suivant de l'arbre
-	searchBest(Joueur, Moves, Adversaire, Profondeur1, Alpha1, Beta1, nil, OriginalAdversaire, [Move, Value]).
+	Profr1 is Prof - 1,	% on passe au niveau suivant de l'arbre
+	searchBest(Joueur, Mvmt, Adv, Prof1, Alpha1, Beta1, nil, OAdv, [Move, Value]).
 
-% searchBest(+Joueur,+Moves,+Adversaire,+Profondeur,+Alpha,+Beta,+R,?BestMove)
+% searchBest(+Joueur, +Adv, +Mvmt, +Profondeur,+Alpha,+Beta,+R,?BestMove)
 % Retourne le meilleur coup à jouer.
-searchBest(_Joueur, [], _Adversaire, _Profondeur, Alpha, _Beta, Move, _, [Move,Alpha]) :- 
+searchBest(_J, _Adv, [], _Prof, Alpha, _Beta, Move, _, [Move,Alpha]) :- 
 	!.
-searchBest(Joueur, [[[Fx,Fy],[Tx,Ty],N]|Moves], Adversaire, Profondeur, Alpha, Beta, R, OriginalAdversaire, BestMove) :-
-	convert(From, Fx, Fy),
-	convert(To, Tx, Ty),
+searchBest(J, Adv, [[X1, X2, X3, X4]|Mvmt], Prof, Alpha, Beta, R, OJ, OAdv, BestMove) :-
 	move(Adversaire, From, To, N, NAdversaire),
 	getAdv(Joueur, OtherJoueurR),
-	alphaBeta(OtherJoueurR, Profondeur, NAdversaire, Alpha, Beta, _OtherCoup, OriginalAdversaire, Value),
+	alphaBeta(OtherJoueurR, Profondeur, NAdversaire, Alpha, Beta, _OtherCoup, OriginalAdversaire, Value)
 	Value1 is -Value,		% negamax
 	cutBranches(Joueur,[[Fx,Fy],[Tx,Ty],N], OriginalAdversaire, Value1,Profondeur,Alpha,Beta,Moves,Adversaire,R,BestMove).
 
