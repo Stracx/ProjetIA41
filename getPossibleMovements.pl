94446c3fe4c07deee39f71aa695948130d334328
Salomé <salome.welche@laposte.net>
2015-06-12 18:06:06 +0200

    alpha beta marche parfaitement, faut pas aller a une profondeur > 5 Corrections et ajouts sur le rapport

--- getPossibleMovements.pl
+++ getPossibleMovements.pl
@@ -1,6 +1,6 @@
 :- include('ordonner.pl').
 
-%getPossibleMovementL(+Joueur, +Joueur, +Adversaire, -ListeMvmt ).
+%getPossibleMovementL(+Joueur, +Adversaire, -ListeMvmt ).
 
  getPossibleMovementL([], A, L) :- listeDepart(LD),subtract(LD,A, L2), ldeL(L2, L), ! .
 getPossibleMovementL([X1], A, L):- getPossibleMovementX(X1, [X1, X1], A, L, 8),!.
@@ -62,7 +62,6 @@ deplacement8(A,B):- B is A-9,B>0,(B mod 10)<6,(B mod 10)>0,!.
 
 
 add(X,L, [X|L]).
-
 
 % fct de swap
 swap([C|R1], A, B, [C|R2]):- C\=A,swap(R1, A, B, R2).
